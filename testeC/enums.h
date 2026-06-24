2a02720064da7e617daf42b958b84a4d81bda50b
Zandiano <126298640+Zandiano@users.noreply.github.com>
2026-06-24 01:41:47 -0300

    commit certa

--- testeC/enums.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENUMS
-#define ENUMS
-
-enum cena{MENU,WORLD_MAP,BATTLE,GAME_END};
-enum element{nullElement,normal,fire,water,eletric,grass,ice,fightining,poison,ground,flying,psychic,bug,rock,ghost,dragon,dark,steel,fairy};
-enum atr{hp, atk, spatk, def, spdef, spd};
-enum type{physical, special};
-
-#endif
